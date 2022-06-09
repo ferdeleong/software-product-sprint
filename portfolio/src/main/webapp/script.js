dc179999e3dcbc08ee450a71df23e2cf4e86f21a
ferdeleong <fernanda.-deleon@hotmail.com>
2022-06-09 00:15:49 +0000

    Transform home page into a template and fix 80 characters long

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const content = {
+    about: {
+      quote: `
+        You can’t keep growing and working on yourself without accepting where you are right now.
+        Acceptance is your key to freedom.
+      `,
+      paragraph: [
+        `
+          I believe that my education in BS Architecture and developed creativeness 
+          combined with Computer Science give me an especially great advantage when 
+          approaching problems or challenges that require creative solutions or to 
+          think out of the box, so I can bring new and fresh ideas to the table
+        `,
+        `
+          What I love about CS is that it gives you the tools to explore and 
+          materialize the vast universe of ideas and thoughts that humans create.
+          I build for those who can’t and need us. I build to promote autonomous 
+          human development, social cohesion and citizen integration through retribution.
+        `,
+        `
+          Im looking forward to create an app to facilitate communication between 
+          foreigners and foreign students to find roommates based on hobbies,
+          life style, habits, etc, and create meaningful connections
+        `
+      ]
+    },
+    projects: [
+      {
+        name: "Hackathon",
+        description: `
+          Developed a decentralized voting system over the Ethereum blockchain 
+          using smart contracts in Solidity. Microservices architecture built with 
+          Node.js, Docker, and IBM Cloud.
+        `,
+      },
+      {
+        name: "Chrome extension",
+        description: `
+          Created a Chrome extension for speed reading  using facial recognition 
+          to control speed, and play/stop the session. The extension was built with React and Javascript. 
+          The ML models use ML5 library to have them web based to ensure fast feedback.
+        `
+      }
+    ],
+    gallery: {
+      description: `
+        I enjoy photographing architecture because it's the combination between math and art. 
+        It’s amazing how it reflects the lifestyle and ideologies of the society at the time 
+        of construction, so everytime you see a building you are looking directly at 
+        a time capsule.
+      `,
+      images: ["image1.jpeg"]
+    },
+    me: {
+      hometown: "Monterrey, México",
+      education: {
+        school: "Tec de Monterrey",
+        degrees: [
+          `BS Architecture (08.2018 - 01.2021)
+          `,
+          `BS Computer Science and Technology (01.2021- 12.2024)
+          `,
+          `English (CEF B2 certification), Spanish (Native)
+          `
+        ]
+      },
+      skills: {
+        languages: ["C++", "Javascript", "Python", "Solidity"],
+        knowledge: ["fullstack web development", "databases", "blockchain"],
+      },
+    },
+};
+  
 /**
  * Adds a random greeting to the page.
  */
@@ -27,47 +100,32 @@ function addRandomGreeting() {
   greetingContainer.innerText = greeting;
 }
 
-const aboutDescrition = `
-“You can’t keep growing and working on yourself without accepting where 
-you are right now. Acceptance is your key to freedom.”
+function loadContent () {
+    //About section
+    const quote = document.getElementById("quote");
+    quote.innerText = content.about.quote;
 
-I believe that my education in BS Architecture and developed creativeness 
-combined with Computer Science  give me an especially great advantage when 
-approaching problems or challenges that require creative solutions or to 
-think out of the box, so I can bring new and fresh ideas to the table
- 
-What I love about CS is that it gives you the tools to explore and 
-materialize the vast universe of ideas and thoughts that humans create. 
-I build for those who can’t and need us. I build to promote autonomous 
-human development, social cohesion and citizen integration through retribution.
- 
-Im looking forward to create an app to facilitate communication between 
-foreigners and foreign students to find roommates based on hobbies,
-life style, habits, etc, and create meaningful connections
-`;
+    const aboutParagraph = document.getElementById("aboutParagraph");
+    aboutParagraph.innerText = content.about.paragraph;
+    
+    //Projects section
+    const project1 = document.getElementById("project");
+    project1.innerText = content.projects;
 
-const projectsDescrition = `
-* Developed a decentralized voting system over the Ethereum blockchain 
-using smart contracts in Solidity. Microservices architecture built with 
-Node.js, Docker, and IBM Cloud.
-* Created a Chrome extension for speed reading  using facial recognition 
-to control speed, and play/stop the session. The extension was built with 
-React and Javascript. 
-The ML models use ML5 library to have them web based to ensure fast feedback.
-`;
+    //Gallery section
+    const galleryDescription = document.getElementById("galleryDescription");
+    galleryDescription.innerText = content.gallery.description;
+    
+    //Me section
+    const hometown = document.getElementById("hometown");
+    hometown.innerText = content.me.hometown;
 
-const galleryDescrition = `
-I enjoy photographing architecture because it's the combination between math and art. 
-It’s amazing how it reflects the lifestyle and ideologies of the society at the time 
-of construction, so everytime you see a building you are looking directly at 
-a time capsule.
-`;
+    //Education sub-section
+    const school = document.getElementById("school");
+    school.innerText = content.me.education.school;
 
-const meDescrition = `
-School: Tec de Monterrey
-Hometown: Monterrey, México 
+    //Skills sub-section
+    const degrees = document.getElementById("degrees");
+    degrees.innerText = content.me.education.degrees;
 
-BS Architecture (08.2018 - 01.2021)
-BS Computer Science and Technology (01.2021- 12.2024)
-English (CEF B2 certification), Spanish (Native)
-`;
+}
